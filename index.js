2f732b9be942d26812f9b32de50623ac25a3a518
Gabriel Valle <gvalle91@gmail.com>
2018-05-24 20:29:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdowwn(days, holiday){
+function holidayCountdowwn(holiday, days){
   return `It's ${days} days until ${holiday}!`
 }
